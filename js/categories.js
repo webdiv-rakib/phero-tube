b37cf5a7b36dd367a4f4b4d7907092dfb3b5d5c8
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-01-26 03:08:17 +0600

    dynamically display category.

--- js/categories.js
+++ js/categories.js
@@ -2,3 +2,41 @@ console.log('video script added');
 /**
  * 1. Fetch, Lead and Show Categories on html
  */
+
+// create loadCategories
+const loadCategories = () => {
+    // fetch the data
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+        .then(res => res.json())
+        .then(data => displayCategories(data.categories))
+        .catch(err => console.log(err))
+}
+
+// category
+// : 
+// "Music"
+// category_id
+// : 
+// "1001"
+// [[Prototype]]
+// : 
+// Object
+
+// create displayCategories
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById('categories');
+    for (const item of categories) {
+        console.log(item)
+        // create a button for each category
+        const button = document.createElement('button');
+        button.classList = 'btn bg-[#d3d3d3]';
+        button.innerText = item.category;
+
+        // add button to category container
+        categoryContainer.appendChild(button);
+    }
+}
+
+
+
+loadCategories();
