574ed6164c2120b51bbc98e6a461340ca348b3c0
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-01-26 23:19:43 +0600

    video card design complete

--- js/video.js
+++ js/video.js
@@ -18,14 +18,16 @@ const displayVideos = (videos) => {
                 src=${video.thumbnail}
             alt="Shoes" />
          </figure>
-        <div class="card-body flex flex-row">
+        <div class="card-body flex flex-row gap-5">
             <div>
                 <img src=${video.authors[0].profile_picture} class="h-10 w-10 rounded-full" />
             </div>
             <div>
                 <h2 class="font-bold">${video.title}</h2>
-                <p>${video.authors[0].profile_name}</p>
-                <img src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000" class="w-5"/>
+                <div class="flex flex-row justify-center items-center gap-3">
+                    <p>${video.authors[0].profile_name}</p>
+                    <img src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000" class="w-4 h-4"/>
+                </div>
             </div>
         </div>
         `
